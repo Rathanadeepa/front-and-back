975bff89f2dff834ba7b1e329b5da16790caf781
Rathanadeepa <deeparaj@2401@gmail.com>
2023-12-29 12:47:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
+import {API_URL_DELETE, API_URL_GET, API_URL_POST} from "./url/url"
+function Parent_data() {
+  const [collect,setcollect]=useState([]);
+  const [name,setname]=useState("");
+  const [email,setemail]=useState("");
+  const [ref,setref]=useState(true);
+  useEffect(()=>{
+    axios.get(API_URL_GET).then((res)=>{console.log(res.data)
+      setcollect(res.data);
+    })
+    
+  },[ref]);
+  function handle(){
+    axios.post(API_URL_POST,{
+      name,
+      email,
+    });
+};
+let update =async(v)=>{
+  setname(v.name);
+  setemail(v.email);
+};
+let delete_data =(v)=>{
+   axios.delete(`${API_URL_DELETE}/${v._id}`).then((res)=>{
+    setref(!ref);
+   });
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return(<>
+  <h1>FRONTEND </h1>
+  <form>
+    <span>name:</span>
+    <input type="text" value={name} onChange={(e)=>{setname(e.target.value)}}/>
+    <br></br>
+    <span>email:</span>
+    <input type="text" value={email} onChange={(e)=>{setemail(e.target.value)}}/>
+    <button onClick={handle}>login</button>
+  </form>
+  <h1>backend</h1>
+  <div>
+    {
+      collect && collect.map((v,i)=>(
+      <div key={i}> {v.name}{v.email}
+        <button onClick={()=>{update(v)}}>update</button>
+        <button onClick={()=>{delete_data(v)}}>delete</button>
+        </div>
+      ))
+    }
+  </div>
+  </>)
 }
-
-export default App;
+export default Parent_data;
---
+++ src/url/url.js
@@ -0,0 +1,5 @@
+const API_URL_GET="http://localhost:6004/get";
+const API_URL_POST ="http://localhost:6004/create"
+
+const API_URL_DELETE="http://localhost:6004/delete"
+export  {API_URL_GET,API_URL_POST,API_URL_DELETE}
